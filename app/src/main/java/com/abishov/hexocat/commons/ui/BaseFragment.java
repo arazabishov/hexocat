73e82ff25967c16c39a25b240b83ea92ee08befd
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-07-13 13:17:52 +0400

    BaseFragment which makes use of leak canary.

---
+++ app/src/main/java/com/abishov/hexocat/commons/ui/BaseFragment.java
@@ -0,0 +1,34 @@
+package com.abishov.hexocat.commons.ui;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.abishov.hexocat.Hexocat;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public abstract class BaseFragment extends Fragment {
+
+    @Nullable
+    private Unbinder unbinder;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        // check if instance of fragment is leaked or not
+        ((Hexocat) getActivity().getApplicationContext())
+                .refWatcher().watch(this);
+
+        // unbind butterknife
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
+
+    protected void bind(@NonNull Object target, @NonNull android.view.View view) {
+        unbinder = ButterKnife.bind(target, view);
+    }
+}
