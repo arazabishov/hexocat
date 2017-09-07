d46efab1ec2e5bf653f4e3daeab818e143bbe59d
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-09-07 18:51:16 +0200

    Introduced CircleTransformation class (stolen from u2020).

---
+++ app/src/main/java/com/abishov/hexocat/commons/views/CircleTransformation.java
@@ -0,0 +1,41 @@
+package com.abishov.hexocat.commons.views;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.squareup.picasso.Transformation;
+
+import static android.graphics.Bitmap.Config.ARGB_8888;
+import static android.graphics.Paint.ANTI_ALIAS_FLAG;
+import static android.graphics.Shader.TileMode.CLAMP;
+
+public final class CircleTransformation implements Transformation {
+    public CircleTransformation() {
+    }
+
+    @Override
+    public Bitmap transform(Bitmap bitmap) {
+        int size = bitmap.getWidth();
+        Bitmap rounded = Bitmap.createBitmap(size, size, ARGB_8888);
+        Canvas canvas = new Canvas(rounded);
+
+        BitmapShader shader = new BitmapShader(bitmap, CLAMP, CLAMP);
+        Paint shaderPaint = new Paint(ANTI_ALIAS_FLAG);
+        shaderPaint.setShader(shader);
+
+        RectF rect = new RectF(0, 0, size, size);
+        float radius = size / 2f;
+        canvas.drawRoundRect(rect, radius, radius, shaderPaint);
+
+        bitmap.recycle();
+        return rounded;
+    }
+
+    @Override
+    public String key() {
+        return "circle";
+    }
+}
