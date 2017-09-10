ee6241242c5e3866337c811be1a460a613221911
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-09-10 16:17:57 +0200

    Reduced visibility scope of the TrendingViewState class.

--- app/src/main/java/com/abishov/hexocat/home/trending/TrendingViewState.java
+++ app/src/main/java/com/abishov/hexocat/home/trending/TrendingViewState.java
@@ -11,31 +11,31 @@ import java.util.Collections;
 import java.util.List;
 
 @AutoValue
-public abstract class TrendingViewState implements ViewState, Parcelable {
+abstract class TrendingViewState implements ViewState, Parcelable {
 
     @NonNull
-    public abstract List<RepositoryViewModel> items();
+    abstract List<RepositoryViewModel> items();
 
     @NonNull
-    public static TrendingViewState idle() {
+    static TrendingViewState idle() {
         return new AutoValue_TrendingViewState(true, false, false, false, "",
                 Collections.unmodifiableList(Collections.emptyList()));
     }
 
     @NonNull
-    public static TrendingViewState progress() {
+    static TrendingViewState progress() {
         return new AutoValue_TrendingViewState(false, true, false, false, "",
                 Collections.unmodifiableList(Collections.emptyList()));
     }
 
     @NonNull
-    public static TrendingViewState success(@NonNull List<RepositoryViewModel> items) {
+    static TrendingViewState success(@NonNull List<RepositoryViewModel> items) {
         List<RepositoryViewModel> deepCopy = Collections.unmodifiableList(new ArrayList<>(items));
         return new AutoValue_TrendingViewState(false, false, true, false, "", deepCopy);
     }
 
     @NonNull
-    public static TrendingViewState failure(@NonNull Throwable throwable) {
+    static TrendingViewState failure(@NonNull Throwable throwable) {
         return new AutoValue_TrendingViewState(false, false, false, true, throwable.getMessage(),
                 Collections.unmodifiableList(Collections.emptyList()));
     }
