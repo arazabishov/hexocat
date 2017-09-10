7c8095e874ce2ab6ebb9b620bc7a1b6d69a24dac
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-09-10 12:47:00 +0200

    Changed TrendingView to work with the TrendingViewState model.

--- app/src/main/java/com/abishov/hexocat/home/trending/TrendingFragment.java
+++ app/src/main/java/com/abishov/hexocat/home/trending/TrendingFragment.java
@@ -2,6 +2,7 @@ package com.abishov.hexocat.home.trending;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,7 +15,6 @@ import android.widget.Toast;
 import com.abishov.hexocat.Hexocat;
 import com.abishov.hexocat.R;
 import com.abishov.hexocat.commons.views.BaseFragment;
-import com.abishov.hexocat.commons.views.ViewState;
 import com.squareup.picasso.Picasso;
 
 import javax.annotation.Nonnull;
@@ -26,17 +26,29 @@ import io.reactivex.functions.Consumer;
 public final class TrendingFragment extends BaseFragment implements TrendingView {
     public static final String TAG = TrendingFragment.class.getSimpleName();
 
+    private static final String STATE_VIEW = "state:trendingViewState";
+    private static final String STATE_RECYCLER_VIEW = "state:trendingRecyclerViewState";
+
     @BindView(R.id.recyclerview_trending)
     RecyclerView recyclerViewTrending;
-
-    @Nullable
-    RepositoryAdapter repositoryAdapter;
 
     @Inject
     TrendingPresenter trendingPresenter;
 
     @Inject
     Picasso picasso;
+
+    @Nullable
+    private RepositoryAdapter repositoryAdapter;
+
+    @Nullable
+    private TrendingViewState viewState;
+
+    @Nullable
+    private Parcelable recyclerViewState;
+
+    @Nullable
+    private RecyclerView.LayoutManager recyclerViewLayoutManager;
 
     @Nonnull
     public static TrendingFragment create() {
@@ -60,32 +72,48 @@ public final class TrendingFragment extends BaseFragment implements TrendingView
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         bind(this, view);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewLayoutManager = new LinearLayoutManager(getActivity());
         repositoryAdapter = new RepositoryAdapter(LayoutInflater.from(getActivity()), picasso);
-        recyclerViewTrending.setLayoutManager(layoutManager);
+        recyclerViewTrending.setLayoutManager(recyclerViewLayoutManager);
         recyclerViewTrending.setAdapter(repositoryAdapter);
+
+        if (savedInstanceState != null) {
+            viewState = savedInstanceState.getParcelable(STATE_VIEW);
+            recyclerViewState = savedInstanceState.getParcelable(STATE_RECYCLER_VIEW);
+        }
+
+        trendingPresenter.onAttach(this, viewState);
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onSaveInstanceState(Bundle outState) {
+        if (viewState != null) {
+            outState.putParcelable(STATE_VIEW, viewState);
+            outState.putParcelable(STATE_RECYCLER_VIEW,
+                    recyclerViewLayoutManager.onSaveInstanceState());
+        }
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
         trendingPresenter.onDetach();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        trendingPresenter.onAttach(this);
     }
 
     @Nonnull
     @Override
-    public Consumer<ViewState<RepositoryViewModel>> renderRepositories() {
+    public Consumer<TrendingViewState> renderRepositories() {
         return state -> {
+            viewState = state;
+
             if (state.isSuccess()) {
+                // use diff util to update the list
                 repositoryAdapter.swap(state.items());
             } else if (state.isFailure()) {
                 Toast.makeText(getActivity(),
@@ -96,6 +124,11 @@ public final class TrendingFragment extends BaseFragment implements TrendingView
             } else if (state.isIdle()) {
                 Toast.makeText(getActivity(),
                         "Idle", Toast.LENGTH_SHORT).show();
+            }
+
+            if (recyclerViewState != null) {
+                recyclerViewLayoutManager.onRestoreInstanceState(recyclerViewState);
+                recyclerViewState = null;
             }
         };
     }
--- app/src/main/java/com/abishov/hexocat/home/trending/TrendingView.java
+++ app/src/main/java/com/abishov/hexocat/home/trending/TrendingView.java
@@ -1,7 +1,6 @@
 package com.abishov.hexocat.home.trending;
 
 import com.abishov.hexocat.commons.views.View;
-import com.abishov.hexocat.commons.views.ViewState;
 
 import javax.annotation.Nonnull;
 
@@ -10,5 +9,5 @@ import io.reactivex.functions.Consumer;
 interface TrendingView extends View {
 
     @Nonnull
-    Consumer<ViewState<RepositoryViewModel>> renderRepositories();
+    Consumer<TrendingViewState> renderRepositories();
 }
