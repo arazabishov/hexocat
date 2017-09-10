002c048ae18e22901cba9c0c0d6ddbcc3a28debb
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-09-10 12:47:59 +0200

    Code formatting in RepositoryViewModel.

--- app/src/main/java/com/abishov/hexocat/home/trending/RepositoryViewModel.java
+++ app/src/main/java/com/abishov/hexocat/home/trending/RepositoryViewModel.java
@@ -1,11 +1,12 @@
 package com.abishov.hexocat.home.trending;
 
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import com.google.auto.value.AutoValue;
 
 @AutoValue
-abstract class RepositoryViewModel {
+abstract class RepositoryViewModel implements Parcelable {
 
     @NonNull
     abstract String name();
@@ -17,8 +18,8 @@ abstract class RepositoryViewModel {
     abstract String avatarUrl();
 
     @NonNull
-    static RepositoryViewModel create(@NonNull String name, @NonNull String description,
-                                      @NonNull String ownerAvatar) {
+    static RepositoryViewModel create(@NonNull String name,
+            @NonNull String description, @NonNull String ownerAvatar) {
         return new AutoValue_RepositoryViewModel(name, description, ownerAvatar);
     }
 }
