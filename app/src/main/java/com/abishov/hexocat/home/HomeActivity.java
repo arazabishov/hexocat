e5bdff84f6e715cfb38b27ea403cc385d37641c0
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-10-14 16:57:11 +0200

    Updated HomeActivity to work with fragment manager again.

--- app/src/main/java/com/abishov/hexocat/home/HomeActivity.java
+++ app/src/main/java/com/abishov/hexocat/home/HomeActivity.java
@@ -3,20 +3,16 @@ package com.abishov.hexocat.home;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.abishov.hexocat.R;
+import com.abishov.hexocat.home.trending.TrendingPagerFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public final class HomeActivity extends AppCompatActivity {
-    private static final String STATE_MENU_ITEM_ID = "state:menuItemId";
-
-    @BindView(R.id.framelayout_content)
-    ViewGroup contentView;
 
     @BindView(R.id.bottom_navigation_home)
     BottomNavigationView bottomNavigationView;
@@ -28,26 +24,25 @@ public final class HomeActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
-                item -> swapView(item.getItemId()));
-
-        int selectedNavigationItemId = savedInstanceState == null ?
-                R.id.item_trending : savedInstanceState.getInt(STATE_MENU_ITEM_ID);
-        swapView(selectedNavigationItemId);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt(STATE_MENU_ITEM_ID, bottomNavigationView.getSelectedItemId());
-        super.onSaveInstanceState(outState);
-    }
-
-    private boolean swapView(@IdRes int viewId) {
-        contentView.removeAllViews();
-        if (viewId == R.id.item_trending) {
-            View trendingPagerView = getLayoutInflater().inflate(
-                    R.layout.trending_pager_view, contentView, false);
-            contentView.addView(trendingPagerView);
+                item -> swapFragment(item.getItemId()));
+        if (savedInstanceState == null) {
+            swapFragment(R.id.item_trending);
         }
+    }
+
+    private boolean swapFragment(@IdRes int viewId) {
+        Fragment fragment;
+
+        if (viewId == R.id.item_trending) {
+            fragment = TrendingPagerFragment.create();
+        } else {
+            fragment = new Fragment();
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                .replace(R.id.framelayout_content, fragment)
+                .commitNow();
 
         return true;
     }
