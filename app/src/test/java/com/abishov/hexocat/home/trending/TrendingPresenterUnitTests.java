7cf46e0ed9251d5c72f5225f1ac0e038148878d6
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-10-23 15:59:34 +0200

    Updated unit tests to match new implementation.

--- app/src/test/java/com/abishov/hexocat/home/trending/TrendingPresenterUnitTests.java
+++ app/src/test/java/com/abishov/hexocat/home/trending/TrendingPresenterUnitTests.java
@@ -1,8 +1,10 @@
 package com.abishov.hexocat.home.trending;
 
+import com.abishov.hexocat.commons.schedulers.TrampolineSchedulersProvider;
 import com.abishov.hexocat.github.Repository;
-import com.abishov.hexocat.home.repository.RepositoryViewModel;
 import com.abishov.hexocat.github.User;
+import com.abishov.hexocat.github.filters.SearchQuery;
+import com.abishov.hexocat.home.repository.RepositoryViewModel;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +15,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.threeten.bp.LocalDate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,12 +26,13 @@ import io.reactivex.subjects.BehaviorSubject;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class TrendingPresenterUnitTests {
 
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private TrendingView trendingView;
+    private TrendingContract.View trendingView;
 
     @Mock
     private TrendingRepository trendingRepository;
@@ -36,7 +40,8 @@ public class TrendingPresenterUnitTests {
     @Captor
     private ArgumentCaptor<TrendingViewState> repositoriesConsumer;
 
-    // Faking behaviour of the trendingRepository.
+    // Faking behaviour of the TrendingRepository and TrendingContract.View.
+    private BehaviorSubject<SearchQuery> viewQueries;
     private BehaviorSubject<List<Repository>> listResults;
 
     // Some dummy data.
@@ -45,16 +50,17 @@ public class TrendingPresenterUnitTests {
     // ViewModels corresponding to dummy data.
     private List<RepositoryViewModel> repositoryViewModels;
 
-    // Class under testing.
     private TrendingPresenter trendingPresenter;
+    private SearchQuery searchQuery;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
+        viewQueries = BehaviorSubject.create();
         listResults = BehaviorSubject.create();
-//        trendingPresenter = new TrendingPresenter(
-//                new TrampolineSchedulersProvider(), trendingRepository, 7);
+        trendingPresenter = new TrendingPresenter(
+                new TrampolineSchedulersProvider(), trendingRepository);
 
         User owner = User.create("test_login", "test_html_url", "test_avatar_url");
         repositories = Arrays.asList(
@@ -68,14 +74,20 @@ public class TrendingPresenterUnitTests {
                 RepositoryViewModel.create("test_repository_two", "test_description_two",
                         "7", "11", "test_avatar_url", "test_login"));
 
-        // when(trendingRepository.trendingRepositories(7)).thenReturn(listResults);
+        searchQuery = new SearchQuery.Builder()
+                .createdSince(LocalDate.parse("2017-08-10"))
+                .build();
+
+        when(trendingView.searchQueries()).thenReturn(viewQueries);
+        when(trendingRepository.trendingRepositories(searchQuery)).thenReturn(listResults);
     }
 
     @Test
     public void presenterMustPropagateCorrectStatesOnSuccess() throws Exception {
-        // trendingPresenter.onAttach(trendingView, null);
-        assertThat(listResults.hasObservers()).isTrue();
+        trendingPresenter.onAttach(trendingView);
+        assertThat(viewQueries.hasObservers()).isTrue();
 
+        viewQueries.onNext(searchQuery);
         listResults.onNext(repositories);
         listResults.onComplete();
 
@@ -102,9 +114,10 @@ public class TrendingPresenterUnitTests {
 
     @Test
     public void presenterMustPropagateCorrectStatesOnFailure() throws Exception {
-        // trendingPresenter.onAttach(trendingView);
-        assertThat(listResults.hasObservers()).isTrue();
+        trendingPresenter.onAttach(trendingView);
+        assertThat(viewQueries.hasObservers()).isTrue();
 
+        viewQueries.onNext(searchQuery);
         listResults.onError(new Throwable("test_message"));
         listResults.onComplete();
 
@@ -131,9 +144,10 @@ public class TrendingPresenterUnitTests {
 
     @Test
     public void presenterMustPropagateCorrectStatesWhenNoItems() throws Exception {
-        // trendingPresenter.onAttach(trendingView, null);
-        assertThat(listResults.hasObservers()).isTrue();
+        trendingPresenter.onAttach(trendingView);
+        assertThat(viewQueries.hasObservers()).isTrue();
 
+        viewQueries.onNext(searchQuery);
         listResults.onNext(new ArrayList<>());
         listResults.onComplete();
 
@@ -159,11 +173,14 @@ public class TrendingPresenterUnitTests {
     @Test
     public void presenterMustUnsubscribeFromViewOnDetach() {
         assertThat(listResults.hasObservers()).isFalse();
+        assertThat(viewQueries.hasObservers()).isFalse();
 
-        // trendingPresenter.onAttach(trendingView, null);
-        assertThat(listResults.hasObservers()).isTrue();
+        trendingPresenter.onAttach(trendingView);
+        assertThat(listResults.hasObservers()).isFalse();
+        assertThat(viewQueries.hasObservers()).isTrue();
 
         trendingPresenter.onDetach();
         assertThat(listResults.hasObservers()).isFalse();
+        assertThat(viewQueries.hasObservers()).isFalse();
     }
 }
--- app/src/test/java/com/abishov/hexocat/home/trending/TrendingRepositoryUnitTests.java
+++ app/src/test/java/com/abishov/hexocat/home/trending/TrendingRepositoryUnitTests.java
@@ -3,16 +3,28 @@ package com.abishov.hexocat.home.trending;
 import com.abishov.hexocat.github.Pager;
 import com.abishov.hexocat.github.Repository;
 import com.abishov.hexocat.github.User;
+import com.abishov.hexocat.github.filters.Order;
+import com.abishov.hexocat.github.filters.SearchQuery;
+import com.abishov.hexocat.github.filters.Sort;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.threeten.bp.LocalDate;
 
 import java.util.Arrays;
+import java.util.List;
 
+import io.reactivex.observers.TestObserver;
 import io.reactivex.subjects.PublishSubject;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public final class TrendingRepositoryUnitTests {
@@ -25,8 +37,8 @@ public final class TrendingRepositoryUnitTests {
     // Used to simulate behaviour of the Observable.
     private PublishSubject<Pager<Repository>> subject;
 
-    // The pager with repositories returned by the service.
     private Pager<Repository> repositoryPager;
+    private SearchQuery searchQuery;
 
     @Before
     public void setUp() throws Exception {
@@ -43,25 +55,28 @@ public final class TrendingRepositoryUnitTests {
                 Repository.create("test_repository_two",
                         "test_html_url_two", 4, 3, "test_description_two", owner)));
 
-//        when(trendingService.repositories("created:>2017-08-10")).thenReturn(subject);
-//        when(queryDateProvider.dateBefore(7)).thenReturn("2017-08-10");
+        searchQuery = new SearchQuery.Builder()
+                .createdSince(LocalDate.parse("2017-08-10"))
+                .build();
+
+        when(trendingService.repositories(searchQuery, Sort.STARS, Order.DESC)).thenReturn(subject);
     }
-//
-//    @Test
-//    public void trendingRepositoriesMustCallTrendingServiceWithCorrectFilter() {
-//        TestObserver<List<Repository>> testObserver =
-//                trendingRepository.trendingRepositories(7).test();
-//
-//        subject.onNext(repositoryPager);
-//        subject.onComplete();
-//
-//        testObserver.assertNoErrors();
-//        testObserver.assertComplete();
-//        testObserver.assertValueCount(1);
-//
-//        assertThat(testObserver.values().get(0)).isEqualTo(repositoryPager.items());
-//
-//        verify(queryDateProvider, times(1)).dateBefore(7);
-//        verify(trendingService, times(1)).repositories("created:>2017-08-10");
-//    }
+
+    @Test
+    public void trendingRepositoriesMustCallTrendingServiceWithCorrectFilter() {
+        TestObserver<List<Repository>> testObserver =
+                trendingRepository.trendingRepositories(searchQuery).test();
+
+        subject.onNext(repositoryPager);
+        subject.onComplete();
+
+        testObserver.assertNoErrors();
+        testObserver.assertComplete();
+        testObserver.assertValueCount(1);
+
+        assertThat(testObserver.values().get(0)).isEqualTo(repositoryPager.items());
+
+        verify(trendingService, times(1))
+                .repositories(searchQuery, Sort.STARS, Order.DESC);
+    }
 }
