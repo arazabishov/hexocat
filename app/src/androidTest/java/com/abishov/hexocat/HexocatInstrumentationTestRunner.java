e1ea3b2133a81e72e01517fe70e6d080d948af15
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-11-10 20:58:57 +0100

    hooked-up RxIdler within HexocatInstrumentationTestRunner.

--- app/src/androidTest/java/com/abishov/hexocat/HexocatInstrumentationTestRunner.java
+++ app/src/androidTest/java/com/abishov/hexocat/HexocatInstrumentationTestRunner.java
@@ -5,11 +5,25 @@ import android.app.Instrumentation;
 import android.content.Context;
 import android.support.test.runner.AndroidJUnitRunner;
 
+import com.squareup.rx2.idler.Rx2Idler;
+
+import io.reactivex.plugins.RxJavaPlugins;
+
 public final class HexocatInstrumentationTestRunner extends AndroidJUnitRunner {
 
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context)
             throws InstantiationException, IllegalAccessException, ClassNotFoundException {
         return Instrumentation.newApplication(HexocatInstrumentationTestApp.class, context);
+    }
+
+    @Override
+    public void onStart() {
+        RxJavaPlugins.setInitComputationSchedulerHandler(
+                Rx2Idler.create("rxjava-scheduler-computation"));
+        RxJavaPlugins.setInitIoSchedulerHandler(
+                Rx2Idler.create("rxjava-scheduler-io"));
+
+        super.onStart();
     }
 }
