d6f0b0bce8c29b44f61c4a3e991afa3de5a23be1
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-11-19 15:57:09 +0100

    Hooking-up OkHttpIdlingResource within HexocatTestRunner.

--- app/src/androidTest/java/com/abishov/hexocat/HexocatTestRunner.java
+++ app/src/androidTest/java/com/abishov/hexocat/HexocatTestRunner.java
@@ -4,13 +4,19 @@ import android.app.Application;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.runner.AndroidJUnitRunner;
+import com.jakewharton.espresso.OkHttp3IdlingResource;
 import com.squareup.rx2.idler.Rx2Idler;
 import io.appflate.restmock.RESTMockFileParser;
+import io.appflate.restmock.RESTMockServer;
 import io.appflate.restmock.RESTMockServerStarter;
 import io.appflate.restmock.android.AndroidAssetsFileParser;
 import io.appflate.restmock.android.AndroidLogger;
 import io.reactivex.plugins.RxJavaPlugins;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 
 public final class HexocatTestRunner extends AndroidJUnitRunner {
 
@@ -26,6 +32,11 @@ public final class HexocatTestRunner extends AndroidJUnitRunner {
 
     RESTMockFileParser parser = new AndroidAssetsFileParser(getContext());
     RESTMockServerStarter.startSync(parser, new AndroidLogger());
+    HexocatTestApp.overrideBaseUrl(HttpUrl.parse(RESTMockServer.getUrl()));
+
+    OkHttpClient client = HexocatTestApp.getInstance().appComponent().okHttpClient();
+    IdlingResource okHttpIdlingResource = OkHttp3IdlingResource.create("okhttp", client);
+    Espresso.registerIdlingResources(okHttpIdlingResource);
   }
 
   @Override
