e9a16ddb3778cdfef6792f0516a5675c8b30bd3a
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-11-19 10:40:24 +0100

    Updated HomeScreenTest to use CaptureScreenshotsRule.

--- app/src/androidTest/java/com/abishov/hexocat/home/HomeScreenTest.java
+++ app/src/androidTest/java/com/abishov/hexocat/home/HomeScreenTest.java
@@ -3,48 +3,40 @@ package com.abishov.hexocat.home;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
-
 import com.abishov.hexocat.HexocatInstrumentationTestApp;
-import com.squareup.spoon.SpoonRule;
-
+import com.abishov.hexocat.common.rule.CaptureScreenshots;
+import com.abishov.hexocat.common.rule.CaptureScreenshotsRule;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import okhttp3.mockwebserver.MockWebServer;
-
 public final class HomeScreenTest {
 
-    @Rule
-    public ActivityTestRule<HomeActivity> activityTestRule =
-            new ActivityTestRule<>(HomeActivity.class, false, false);
+  @Rule
+  public ActivityTestRule<HomeActivity> activityTestRule =
+      new CaptureScreenshotsRule<>(HomeActivity.class, false, false);
 
-    @Rule
-    public SpoonRule spoonRule = new SpoonRule();
+  @Rule
+  public MockWebServer mockWebServer = new MockWebServer();
 
-    @Rule
-    public MockWebServer mockWebServer = new MockWebServer();
+  private HomeRobot homeRobot;
 
-    private HomeRobot homeRobot;
+  @Before
+  public void setUp() throws Exception {
+    homeRobot = new HomeRobot();
 
-    @Before
-    public void setUp() throws Exception {
-        homeRobot = new HomeRobot();
+    HexocatInstrumentationTestApp hexocat =
+        (HexocatInstrumentationTestApp) InstrumentationRegistry.getTargetContext()
+            .getApplicationContext();
+    hexocat.overrideBaseUrl(mockWebServer.url("/mock/"));
 
-        HexocatInstrumentationTestApp hexocat = (HexocatInstrumentationTestApp)
-                InstrumentationRegistry.getTargetContext().getApplicationContext();
-        hexocat.overrideBaseUrl(mockWebServer.url("/mock/"));
+    activityTestRule.launchActivity(new Intent());
+  }
 
-        activityTestRule.launchActivity(new Intent());
-    }
-
-    @Test
-    public void clickOnTrendingTabMustNavigateToTrendingScreen() {
-        spoonRule.screenshot(activityTestRule.getActivity(), "state_before");
-
-        homeRobot.navigateToTrendingScreen()
-                .someAction();
-
-        spoonRule.screenshot(activityTestRule.getActivity(), "state_after");
-    }
+  @Test
+  @CaptureScreenshots
+  public void clickOnTrendingTabMustNavigateToTrendingScreen() {
+    homeRobot.navigateToTrendingScreen().someAction();
+  }
 }
