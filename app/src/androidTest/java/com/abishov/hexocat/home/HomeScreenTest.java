ed9ea73adb9ff2d5d74a9f8e4dc88287c3c215a1
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-11-25 21:35:26 +0100

    Matching items from the test payload in tests using RepositoryItemRobot.

--- app/src/androidTest/java/com/abishov/hexocat/home/HomeScreenTest.java
+++ app/src/androidTest/java/com/abishov/hexocat/home/HomeScreenTest.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import com.abishov.hexocat.common.rule.CaptureScreenshots;
 import com.abishov.hexocat.common.rule.CaptureScreenshotsRule;
+import com.abishov.hexocat.home.trending.TrendingRobot;
 import io.appflate.restmock.RESTMockServer;
+import io.appflate.restmock.RequestsVerifier;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,11 +32,26 @@ public final class HomeScreenTest {
 
   @Test
   @CaptureScreenshots
-  public void clickOnTrendingTabMustNavigateToTrendingScreen() {
+  public void mustRenderTrendingRepositoriesForToday() {
     whenGET(pathContains("search/repositories"))
-        .thenReturnFile("search/repositories/200_trending_1.json");
+        .thenReturnFile("search/repositories/200_trending_today.json");
 
     activityRule.launchActivity(new Intent());
-    homeRobot.navigateToTrendingScreen().someAction();
+
+    TrendingRobot trendingRobot = homeRobot.navigateToTrendingScreen();
+
+    trendingRobot.withRepositoryItemAt(0)
+        .withName("charts");
+    trendingRobot.withRepositoryItemAt(1)
+        .withName("state-of-the-art-result-for-machine-learning-problems");
+    trendingRobot.withRepositoryItemAt(2)
+        .withName("hangzhouYunQi2017ppt");
+    trendingRobot.withRepositoryItemAt(3)
+        .withName("bottery");
+    trendingRobot.withRepositoryItemAt(4)
+        .withName("deepo");
+
+    // TODO: refactor TrendingPagerView to load only one page at a time
+    RequestsVerifier.verifyGET(pathContains("search/repositories")).atLeast(1);
   }
 }
