c8a744d37de04afafb2cf2dde9d91246e9b85187
Araz Abishov <araz.abishov.gsoc@gmail.com>
2017-11-25 21:35:00 +0100

    Updated HomeRobot to safely click on BottomNavigationItemView.

--- app/src/androidTest/java/com/abishov/hexocat/home/HomeRobot.java
+++ app/src/androidTest/java/com/abishov/hexocat/home/HomeRobot.java
@@ -1,12 +1,23 @@
 package com.abishov.hexocat.home;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.abishov.hexocat.common.espresso.BottomNavigationViewMatcher.isNavigationItemChecked;
+
+import com.abishov.hexocat.R;
 import com.abishov.hexocat.home.trending.TrendingRobot;
 
 public final class HomeRobot {
 
   public TrendingRobot navigateToTrendingScreen() {
-    // TODO: find a solution for double-click of trending item
-    // onView(withId(R.id.item_trending)).perform(click());
+    try {
+      onView(withId(R.id.item_trending)).check(matches(isNavigationItemChecked()));
+    } catch (AssertionError assertionError) {
+      onView(withId(R.id.item_trending)).perform(click()).check(matches(isNavigationItemChecked()));
+    }
+
     return new TrendingRobot();
   }
 }
