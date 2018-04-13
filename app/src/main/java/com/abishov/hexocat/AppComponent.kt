df1fc1aaeb5a3a3ce6d199819357452a17a8930e
Araz Abishov <araz@abishov.com>
2018-04-13 15:18:15 +0200

    Ports AppComponent to Kotlin

---
+++ app/src/main/java/com/abishov/hexocat/AppComponent.kt
@@ -0,0 +1,50 @@
+package com.abishov.hexocat
+
+import android.app.Application
+import com.abishov.hexocat.common.network.NetworkModule
+import com.abishov.hexocat.common.picasso.PicassoModule
+import com.abishov.hexocat.common.schedulers.SchedulerModule
+import com.squareup.picasso.RequestHandler
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import org.threeten.bp.Clock
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+  modules = arrayOf(
+    AppModule::class,
+    NetworkModule::class,
+    PicassoModule::class,
+    SchedulerModule::class,
+    AndroidInjectionModule::class,
+    AppBindings::class
+  )
+)
+interface AppComponent {
+
+  fun okHttpClient(): OkHttpClient
+
+  fun inject(hexocat: Hexocat)
+
+  @Component.Builder
+  interface Builder {
+
+    @BindsInstance
+    fun clock(clock: Clock): Builder
+
+    @BindsInstance
+    fun baseUrl(baseUrl: HttpUrl): Builder
+
+    @BindsInstance
+    fun application(application: Application): Builder
+
+    @BindsInstance
+    fun requestHandler(requestHandler: RequestHandler?): Builder
+
+    fun build(): AppComponent
+  }
+}
