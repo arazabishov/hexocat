e79649feba10e245f80cd3049d24fb5ac747fd4b
Araz Abishov <araz@abishov.com>
2018-04-13 13:45:31 +0200

    Converts TrendingModule to Kotlin

---
+++ app/src/main/java/com/abishov/hexocat/home/trending/TrendingModule.kt
@@ -0,0 +1,21 @@
+package com.abishov.hexocat.home.trending
+
+import com.abishov.hexocat.common.dagger.FragmentScope
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+
+@Module
+@FragmentScope
+internal class TrendingModule {
+
+  @Provides
+  @FragmentScope
+  internal fun trendingPresenter(impl: TrendingPresenter): TrendingContract.Presenter = impl
+
+  @Provides
+  @FragmentScope
+  internal fun trendingService(retrofit: Retrofit): TrendingService {
+    return retrofit.create(TrendingService::class.java)
+  }
+}
