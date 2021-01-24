68dfa6b9c3d84870aa544f8b6c4c6205c52bbf6f
Araz Abishov <araz@abishov.com>
2021-01-24 20:15:33 +0100

    Updating unit tests and fixing lint warnings

--- app/src/main/java/com/abishov/hexocat/home/trending/Trending.kt
+++ app/src/main/java/com/abishov/hexocat/home/trending/Trending.kt
@@ -1,12 +1,12 @@
 package com.abishov.hexocat.home.trending
 
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
