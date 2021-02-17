c01c1b9a294015ce3b533aed014b6bc5b3b39ef6
Araz Abishov <arabisho@microsoft.com>
2021-02-17 10:21:00 +0100

    Printing logcat logs on CI

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ fi
 # This will: compile the project, run lint, run tests under JVM, package apk,
 # check the code quality and run tests on the device/emulator.
 "$PROJECT_DIR"/gradlew $GRADLE_ARGUMENTS clean build
-"$PROJECT_DIR"/gradlew $GRADLE_ARGUMENTS :android:connectedCheck --info
+"$PROJECT_DIR"/gradlew $GRADLE_ARGUMENTS :android:connectedCheck
